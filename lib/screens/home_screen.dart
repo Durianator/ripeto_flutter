4626f03d6d1a3c3f9e32aa317d438d5c10a11c5a
Durianator <iskandarzhilmi@gmail.com>
2022-01-21 17:09:12 +0800

    Completed notification.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
