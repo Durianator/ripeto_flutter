513c61f25cf8024fe69aa67c9f8dd765b0dae157
Iskandar Zulqarnain Hilmi <iskandarzhilmi@gmail.com>
2022-06-17 15:24:52 +0800

    changed the font

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ripeto_flutter/screens/add_habit_screen.dart';
-import 'package:ripeto_flutter/screens/edit_habit_screen.dart';
 import 'package:ripeto_flutter/screens/home_screen.dart';
 import 'package:ripeto_flutter/screens/login_screen.dart';
 import 'package:ripeto_flutter/screens/progress_screen.dart';
 import 'package:ripeto_flutter/screens/real_home_screen.dart';
 import 'package:ripeto_flutter/screens/settings_screen.dart';
 import 'package:ripeto_flutter/screens/sign_up_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ripeto_flutter/service/notification_api.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
@@ -35,7 +29,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Ripeto',
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(
+        textTheme: GoogleFonts.archivoTextTheme(
           Theme.of(context).textTheme,
         ),
         primarySwatch: Colors.blue,
